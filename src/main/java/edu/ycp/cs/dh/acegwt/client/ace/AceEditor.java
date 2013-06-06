230a48c1e8862e0df7930450461e21e0627e5509
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-07 09:40:03 +1000

    Working on support for misspelled word suggestions

--- src/main/java/edu/ycp/cs/dh/acegwt/client/ace/AceEditor.java
+++ src/main/java/edu/ycp/cs/dh/acegwt/client/ace/AceEditor.java
@@ -758,10 +758,81 @@ public class AceEditor extends Composite implements RequiresResize, IsEditor<Lea
             }
 
             // Add the CSS rules to highlight spelling errors
-            $wnd.jQuery("<style type='text/css'>.ace_marker-layer .misspelled { position: absolute; z-index: -2; background-color: rgba(255, 0, 0, 0.2); }</style>").appendTo("head");
-            $wnd.jQuery("<style type='text/css'>.misspelled { background-color: rgba(255, 0, 0, 0.2); }</style>").appendTo("head");
-			$wnd.jQuery("<style type='text/css'>.ace_marker-layer .badword { position: absolute; z-index: -2; background-color: rgba(245, 255, 0, 0.2); }</style>").appendTo("head");
-			$wnd.jQuery("<style type='text/css'>.badword { background-color: rgba(245, 255, 0, 0.2); }</style>").appendTo("head");
+            $wnd.jQuery("<style type='text/css'>.ace_marker-layer div[class^='.misspelled'] { position: absolute; z-index: -2; background-color: rgba(255, 0, 0, 0.2); }</style>").appendTo("head");
+            $wnd.jQuery("<style type='text/css'>div[class^='.misspelled'] { background-color: rgba(255, 0, 0, 0.2); }</style>").appendTo("head");
+			$wnd.jQuery("<style type='text/css'>.ace_marker-layer div[class^='.badword'] { position: absolute; z-index: -2; background-color: rgba(245, 255, 0, 0.2); }</style>").appendTo("head");
+			$wnd.jQuery("<style type='text/css'>div[class^='.badword'] { background-color: rgba(245, 255, 0, 0.2); }</style>").appendTo("head");
+
+            // Setup the content menu
+			var replaceWord = function(original, line, start, end, replacement) {
+				var lines = original.split("\n");
+				var output = "";
+				for (var i = 0, _len = lines.length; i < _len; ++i) {
+					if (i != line) {
+						output += lines[i] + (i == _len - 1 ? "" : "\n");
+					} else {
+						output += lines[i].substring(0, start);
+						output += replacement;
+						output += lines[i].substring(end, lines[i].length) + (i == _len - 1 ? "" : "\n");
+					}
+				}
+
+				return output;
+			}
+
+			$wnd.jQuery(editor).contextMenu(function() {
+				var retValue = [];
+				var word = editor.getSession().getValue().split("\n")[this.wordData.line].substring(this.wordData.start, this.wordData.end);
+				var suggestions = dictionary.suggest(word);
+
+				for (var i = 0, _len = suggestions.length; i < _len; i++) {
+					var option = {};
+					var suggestion = suggestions[i];
+					option[suggestion]=function(suggestion, wordData){
+						return function(menuItem,menu){
+							editor.getSession().setValue(
+								replaceWord(
+									editor.getSession().getValue(),
+									wordData.line,
+									wordData.start,
+									wordData.end,
+									suggestion))
+						};
+					}(suggestion, this.wordData);
+
+                    retValue.push(option);
+				}
+
+				return retValue;
+			}, {theme:'human', beforeShow: function() {
+
+				var retValue = false;
+
+				this.wordData = {};
+
+				$wnd.jQuery("div[class^='misspelled']").each(
+					function(wordData){
+						return function(){
+							// Collision detection code - see if the mouse was over a div with the misselled class
+                            if ($wnd.jQuery(this).offset().left <= event.clientX &&
+								$wnd.jQuery(this).offset().left + $wnd.jQuery(this).width() >= event.clientX &&
+								$wnd.jQuery(this).offset().top <= event.clientY &&
+								$wnd.jQuery(this).offset().top + $$wnd.jQuery(this).height() >= event.clientY) {
+								retValue = true;
+
+								var matches = /misspelled-(\d+)-(\d+)-(\d+)/.exec($wnd.jQuery(this).attr('class'));
+                                if (matches.length >= 4) {
+                                    wordData['line'] = matches[1];
+                                    wordData['start'] = matches[2];
+                                    wordData['end'] = matches[3];
+                                }
+
+							}
+						};
+					}(this.wordData));
+
+				return retValue;
+			}});
 
             var contentsModified = true;
 
@@ -905,12 +976,24 @@ public class AceEditor extends Composite implements RequiresResize, IsEditor<Lea
                         //}
                         for (var j in misspellings.misspelled) {
                             var range = new Range(i, misspellings.misspelled[j][0], i, misspellings.misspelled[j][1]);
-                            markersPresent[markersPresent.length] = session.addMarker(range, "misspelled", "typo", true);
+
+                            // Add the information required to identify the misspelled word to the class itself. This
+                            // gives us a way to go back from a click event to a word.
+
+                            markersPresent[markersPresent.length] = session.addMarker(
+                                range,
+                                "misspelled-" + i + "-" + misspellings.misspelled[j][0] + "-" + misspellings.misspelled[j][1],
+                                "typo",
+                                true);
                         }
 
 						for (var j in misspellings.badWords) {
 							var range = new Range(i, misspellings.badWords[j][0], i, misspellings.badWords[j][1]);
-							markersPresent[markersPresent.length] = session.addMarker(range, "badword", "typo", true);
+							markersPresent[markersPresent.length] = session.addMarker(
+                                range,
+                                "badword-" + i + "-" + misspellings.badWords[j][0] + "-" + misspellings.badWords[j][1],
+                                "typo",
+                                true);
 						}
                     }
                 } finally {
