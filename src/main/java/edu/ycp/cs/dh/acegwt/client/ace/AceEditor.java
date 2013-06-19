354ddc5c51c700b5c6672d7e6d204babfb6fc3e1
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-19 13:45:44 +1000

    Moved spellchecking into background thread

--- src/main/java/edu/ycp/cs/dh/acegwt/client/ace/AceEditor.java
+++ src/main/java/edu/ycp/cs/dh/acegwt/client/ace/AceEditor.java
@@ -967,194 +967,77 @@ public class AceEditor extends Composite implements RequiresResize, IsEditor<Lea
 			}});
 
             var contentsModified = true;
+			var currentlySpellchecking = false;
+			var markersPresent = [];
 
             // Check for changes to the text
             editor.getSession().on('change', function(e) {
                 contentsModified = true;
             });
 
-            // Check the spelling of a line, and return [start, end]-pairs for misspelled words.
-            var misspelled = function(line) {
-
-
-                // remove all xml/html elements
-                var tagRe = /<.*?>/;
-                var tagMatch = null;
-                while ((tagMatch = line.match(tagRe)) != null) {
-                    var tagLength = tagMatch[0].length;
-                    var replacementString = "";
-                    for (var i = 0; i < tagLength; ++i) {
-                        replacementString += " ";
+            // Setup a worker to perform the spell checking, and handle the results
+            var spellingWorker = new Worker("javascript/typojs/checkspelling.js");
+			// Set the dictionaries.
+            spellingWorker.postMessage({
+				positiveDictionary: positiveDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()(),
+				negativeDictionary: negativeDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()(),
+				negativePhraseDictionary: negativePhraseDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()()});
+            spellingWorker.addEventListener('message', function(e){
+				try {
+					if (editor == null) {
+                        return;
                     }
-                    line = line.replace(tagRe, replacementString);
-                }
 
-                // remove all xml/html entities
-                var entityRe = /&.*?;/;
-                var entityMatch = null;
-                while ((entityMatch = line.match(entityRe)) != null) {
-                    var entityLength = entityMatch[0].length;
-                    var replacementString = "";
-                    for (var i = 0; i < entityLength; ++i) {
-                        replacementString += " ";
-                    }
-                    line = line.replace(entityRe, replacementString);
-                }
+                    var lineData = e.data;
 
-                // remove all urls
-                var urlRe = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
-                var urlMatch = null;
-                while ((urlMatch = line.match(urlRe)) != null) {
-                    var urlLength = urlMatch[0].length;
-                    var replacementString = "";
-                    for (var i = 0; i < urlLength; ++i) {
-                        replacementString += " ";
-                    }
-                    line = line.replace(urlRe, replacementString);
-                }
+					var session = editor.getSession();
 
-                // remove all numbers
-                var numberRe = /\b\d+\b/;
-                var numberMatch = null;
-                while ((numberMatch = line.match(numberRe)) != null) {
-                    var numberLength = numberMatch[0].length;
-                    var replacementString = "";
-                    for (var i = 0; i < numberLength; ++i) {
-                        replacementString += " ";
-                    }
-                    line = line.replace(numberRe, replacementString);
-                }
+					// Clear the markers.
+					for (var i in markersPresent) {
+						session.removeMarker(markersPresent[i]);
+					}
+					markersPresent = [];
 
-                // replace any character that doesn't make up a word with a space, and then split on space
-				var phraseWords = line.split(/\s/);
-                var words = line.replace(/[^a-zA-Z0-9'\\-]/g, ' ').split(/\s/);
+					var Range = $wnd.ace.require('ace/range').Range;
 
-                var misspelled = [];
-                var badWords = [];
-                var badPhrases = [];
-                var testedWords = [];
+                    for (var lineDataIndex = 0, lineDataLength = lineData.length; lineDataIndex < lineDataLength; ++lineDataIndex) {
+                        var misspellings = lineData[lineDataIndex];
 
-				for (var wordIndex = 0, wordCount = phraseWords.length; wordIndex < wordCount; ++wordIndex) {
-					testedWords.push(false);
-                }
+                        for (var j in misspellings.misspelled) {
+                            var range = new Range(lineDataIndex, misspellings.misspelled[j][0], lineDataIndex, misspellings.misspelled[j][1]);
 
-                // How many words can appear in a phrase that will be checked against
-                // the dictionaries
-                var maxWordsInPhrase = 7;
-
-                outerloop:
-                for (var wordGroupIndex = maxWordsInPhrase; wordGroupIndex > 0; --wordGroupIndex) {
-					var i = 0;
-
-                    // When checking single words, use the words array. Otherwise use the phraseWords array.
-                    var checkArray =  wordGroupIndex == 1 ? words : phraseWords;
-
-                    var lastCheckedWord = 0;
-
-                    innerloop:
-                    for (var wordIndex = 0, wordCount = checkArray.length - wordGroupIndex + 1; wordIndex < wordCount; ++wordIndex) {
-
-                        // do this here so the continues down below don't stop us incrementing the value
-                        var firstWordLengthWithSpace = checkArray[wordIndex].length + 1;
-                        i += firstWordLengthWithSpace;
-
-                        if (wordIndex < lastCheckedWord) {
-                            continue;
+                            // Add the information required to identify the misspelled word to the class itself. This
+                            // gives us a way to go back from a click event to a word.
+                            markersPresent[markersPresent.length] = session.addMarker(
+                                range,
+                                "misspelled-" + lineDataIndex + "-" + misspellings.misspelled[j][0] + "-" + misspellings.misspelled[j][1],
+                                "typo",
+                                true);
                         }
 
-                        var checkWord = "";
+                        for (var j in misspellings.badWords) {
+                            var range = new Range(lineDataIndex, misspellings.badWords[j][0], lineDataIndex, misspellings.badWords[j][1]);
+                            markersPresent[markersPresent.length] = session.addMarker(
+                                range,
+                                "badword-" + lineDataIndex + "-" + misspellings.badWords[j][0] + "-" + misspellings.badWords[j][1],
+                                "typo",
+                                true);
+                        }
 
-						for (var checkWordIndex = wordIndex, checkWordIndexMax = wordIndex + wordGroupIndex; checkWordIndex < checkWordIndexMax; ++checkWordIndex) {
+                        for (var j in misspellings.badPhrases) {
+                            var range = new Range(lineDataIndex, misspellings.badPhrases[j][0], lineDataIndex, misspellings.badPhrases[j][1]);
+                            markersPresent[markersPresent.length] = session.addMarker(
+                                range,
+                                "badphrase-" + lineDataIndex + "-" + misspellings.badPhrases[j][0] + "-" + misspellings.badPhrases[j][1],
+                                "typo",
+                                true);
+                        }
 
-                            if (testedWords[checkWordIndex]) {
-								continue innerloop;
-                            }
-
-                            if (checkArray[checkWordIndex].length == 0) {
-								continue innerloop;
-                            }
-
-                            if (checkWordIndex != wordIndex) {
-								checkWord += " ";
-                            }
-                            checkWord += checkArray[checkWordIndex];
-						}
-
-						// skip non word characters at the start and end of the word or phrase
-						var match = checkWord.match(/^[^a-zA-Z0-9]+/);
-						var startingWhitespace = match != null ? match[0].length : 0;
-
-						var endMatch = checkWord.match(/[^a-zA-Z0-9]+$/);
-						var endingWhitespace = endMatch != null ? endMatch[0].length : 0;
-
-                        // subtract firstWordLengthWithSpace to account for the fact that it was added
-                        // at the start of the loop
-						var start = i + startingWhitespace - firstWordLengthWithSpace;
-						var end = i + checkWord.length - firstWordLengthWithSpace - endingWhitespace;
-
-						if (start < end && checkWord.trim().length != 0) {
-
-							var wordConsumed = false;
-
-                            if (negativePhraseDictionary != null && negativePhraseDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()().check(checkWord.trim())) {
-								wordConsumed = true;
-								badPhrases[badPhrases.length] = [start, end];
-                            } else if (negativeDictionary != null && negativeDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()().check(checkWord.trim())) {
-								wordConsumed = true;
-                                badWords[badWords.length] = [start, end];
-							} else if (wordGroupIndex == 1) {
-                                // check for double words
-                                if (wordIndex < wordCount - 1 && checkArray[wordIndex+1] == checkWord)  {
-									// don't test the next word
-                                    testedWords[wordIndex] = testedWords[wordIndex + 1] = true;
-                                    // this is a bad phrase
-                                    badPhrases[badPhrases.length] = [start, end + checkArray[wordIndex+1].length + 1];
-                                } else if (!positiveDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()().check(checkWord.trim())) {
-								    misspelled[misspelled.length] = [start, end];
-                                }
-							}
-
-							if (wordConsumed) {
-                                // Words will only fall into one dictionary item. Here we make sure that any words in this negative
-                                // match don't get used again.
-                                for (var checkWordIndex = wordIndex, checkWordIndexMax = wordIndex + wordGroupIndex; checkWordIndex < checkWordIndexMax; ++checkWordIndex) {
-                                    testedWords[checkWordIndex] = true;
-                                }
-
-								lastCheckedWord = wordIndex + wordGroupIndex;
-                            }
-						}
 					}
-                }
-
-//                for (var wordIndex = 0, wordCount = words.length; wordIndex < wordCount; ++wordIndex) {
-//                    var checkWord = words[wordIndex];
-//
-//                    // skip initial whitespace
-//                    var match = checkWord.match(/^\s+/);
-//                    var startingWhitespace = match != null ? match[0].length : 0;
-//
-//                    var start = i + startingWhitespace;
-//                    var end = i + checkWord.length;
-//
-//                    if (start < end && checkWord.trim().length != 0) {
-//
-//                        var negativeCheck = negativeDictionary != null && negativeDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()().check(checkWord.trim());
-//
-//                        if (negativeCheck) {
-//                            badWords[badWords.length] = [start, end];
-//                        } else if (!positiveDictionary.@edu.ycp.cs.dh.acegwt.client.typo.TypoJS::getDictionary()().check(checkWord.trim())) {
-//                            misspelled[misspelled.length] = [start, end];
-//                        }
-//                    }
-//                    i += checkWord.length + 1;
-//                }
-                return {misspelled: misspelled, badWords: badWords, badPhrases: badPhrases};
-
-            }
-
-            var currentlySpellchecking = false;
-            var markersPresent = [];
+				} finally {
+					currentlySpellchecking = false;
+				}
+            });
 
             spellCheck = function() {
                 // Wait for the dictionary to be loaded.
@@ -1174,64 +1057,10 @@ public class AceEditor extends Composite implements RequiresResize, IsEditor<Lea
 
                 console.log("Checking Spelling");
 
-                currentlySpellchecking = true;
-                var session = editor.getSession();
+				currentlySpellchecking = true;
+				contentsModified = false;
 
-                // Clear the markers.
-                for (var i in markersPresent) {
-                    session.removeMarker(markersPresent[i]);
-                }
-                markersPresent = [];
-
-                try {
-                    var Range = $wnd.ace.require('ace/range').Range;
-                    var lines = session.getDocument().getAllLines();
-                    for (var i in lines) {
-                        // Clear the gutter.
-                        //session.removeGutterDecoration(i, "misspelled");
-
-                        // Check spelling of this line.
-                        var misspellings = misspelled(lines[i]);
-
-                        // Add markers and gutter markings.
-                        //if (misspellings.length > 0) {
-                            //session.addGutterDecoration(i, "misspelled");
-                        //}
-                        for (var j in misspellings.misspelled) {
-                            var range = new Range(i, misspellings.misspelled[j][0], i, misspellings.misspelled[j][1]);
-
-                            // Add the information required to identify the misspelled word to the class itself. This
-                            // gives us a way to go back from a click event to a word.
-
-                            markersPresent[markersPresent.length] = session.addMarker(
-                                range,
-                                "misspelled-" + i + "-" + misspellings.misspelled[j][0] + "-" + misspellings.misspelled[j][1],
-                                "typo",
-                                true);
-                        }
-
-						for (var j in misspellings.badWords) {
-							var range = new Range(i, misspellings.badWords[j][0], i, misspellings.badWords[j][1]);
-							markersPresent[markersPresent.length] = session.addMarker(
-                                range,
-                                "badword-" + i + "-" + misspellings.badWords[j][0] + "-" + misspellings.badWords[j][1],
-                                "typo",
-                                true);
-						}
-
-						for (var j in misspellings.badPhrases) {
-							var range = new Range(i, misspellings.badPhrases[j][0], i, misspellings.badPhrases[j][1]);
-							markersPresent[markersPresent.length] = session.addMarker(
-								range,
-								"badphrase-" + i + "-" + misspellings.badPhrases[j][0] + "-" + misspellings.badPhrases[j][1],
-								"typo",
-								true);
-						}
-                    }
-                } finally {
-                    currentlySpellchecking = false;
-                    contentsModified = false;
-                }
+				spellingWorker.postMessage({lines: editor.getSession().getDocument().getAllLines()});
             }
 
             // Enable spell checking on regular intervals
